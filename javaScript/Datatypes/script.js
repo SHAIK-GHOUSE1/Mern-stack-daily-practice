5d8cf141afb2399491b817a246c89a1e153a7c5c
SHAIK-GHOUSE1 <shaikghousemohammed045@gmail.com>
2025-12-21 15:45:04 +0530

    javascript

---
+++ javaScript/Datatypes/script.js
@@ -0,0 +1,17 @@
+let a="hello";
+console.log(a);
+console.log(typeof(a));
+let b=0;
+console.log(b)
+console.log(typeof(b))
+let c=true;
+console.log(c)
+console.log(typeof(c))
+let d=4.5
+console.log(d)
+console.log(typeof(d))
+
+let f=Boolean(a)
+console.log(f)
+console.log(typeof(f))
+
---
+++ javaScript/Introduction/script.js
@@ -0,0 +1,13 @@
+
+let a=10;
+console.log(a);
+a=20;
+console.log(a);
+const b=10;
+console.log(b);
+b=20;
+console.log(b);
+// let is local scope and where as var is global scope both are mutable
+
+
+
