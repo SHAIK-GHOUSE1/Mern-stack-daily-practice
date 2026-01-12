13bb965cbd316867c5b4042ffd59c576b59697b2
SHAIK-GHOUSE1 <shaikghousemohammed045@gmail.com>
2026-01-12 11:03:48 +0530

    operators and conditional statements

---
+++ javaScript/conditional statements/script.js
@@ -0,0 +1,32 @@
+const age=90;
+if(age>18)
+{
+    console.log("eligible to vote");
+
+}
+else{
+    console.log("you cannot vote");
+}
+// if, if else, else if ladder 
+// Ternary operator condition ? True statement :  False statement
+
+let cage=19;
+cage>18 ? console.log("he can vote") : console.log("not eligible to vote")
+
+// switch statement
+const score=80;
+switch(score)
+{
+    case 90:
+        console.log("A+");
+        break;
+    case 80:
+        console.log("A");
+        break;
+    case 70:
+        console.log("B");
+        break;
+    default:
+        console.log("needs to improve");
+
+}
---
+++ javaScript/operators/script.js
@@ -0,0 +1,27 @@
+// let name = "hello"
+// // Assignment operators =
+// // Arithmetic operators -> - + / % * **
+// let a = 10;
+// let b = 20;
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
+// console.log(a**b);
+
+
+// Increment operators (++)
+// Decrement operators (--)
+// Shorthand operators ->  a=a+b;  a+=b;
+// comparison operator == checks values in both variables
+// === > < >= <= != checks datatype in both bariables
+
+// console.log(++a)
+// console.log(++a)
+// Logical operator -> && , ||  and !
+const a=true;
+const b=true;
+let result=a&&b;
+console.log(result);
+
