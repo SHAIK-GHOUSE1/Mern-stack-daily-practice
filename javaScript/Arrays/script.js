400130c5b24e135d0e06240e5ff4ae13a1f7a714
SHAIK-GHOUSE1 <shaikghousemohammed045@gmail.com>
2026-01-12 12:53:04 +0530

    Arrays

---
+++ javaScript/Arrays/script.js
@@ -0,0 +1,19 @@
+let std1=[90,23,"ravi",99]
+console.log(std1);
+console.log(std1[0]);
+console.log(std1[1]);
+console.log(std1[2][1]);
+console.log(std1.length);
+std1[0]="hi"
+console.log(std1.length);
+std1.push(100);
+console.log(std1);
+std1.pop();
+console.log(std1);
+std1.shift();
+console.log(std1);
+std1.unshift("welcome");
+console.log(std1);
+
+
+
---
+++ javaScript/loops/script.js
@@ -0,0 +1,15 @@
+for(let i=1;i<10;i++){
+    console.log("Mohammed", i);
+}
+
+let count=0;
+while (count<5){
+    console.log(count);
+    count+=1;
+}
+
+let value=1;
+do{
+    console.log(value);
+    value+=1;
+}while(value<=5);
